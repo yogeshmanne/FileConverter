88816ba07f655ee2aa0da4ff9a72aab2088e3eb4
venky451 <venkateshwarr600@gmail.com>
2024-07-08 17:23:15 +0530

    abcd

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, rendor_template
+from flask_sqlalchemy import SQLAIchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLALchemy(app)
+
+class Upload(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(50))
+    data = db.Column(db.LargeBinary)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
