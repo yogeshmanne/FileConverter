9a44c2ad66f98e3fef3b768fa38623a1b15dacf1
yogeshmanne <142493676+yogeshmanne@users.noreply.github.com>
2024-07-08 17:57:23 +0530

    up22

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, rendor_template
-from flask_sqlalchemy import SQLAIchemy
+from flask import Flask, render_template,request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLALchemy(app)
+db = SQLAlchemy(app)
 
 class Upload(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,3 +14,9 @@ class Upload(db.Model):
 @app.route('/', methods=['GET','POST'])
 def index():
     if request.method == 'POST':
+        file=request.file['file']
+        upload = Upload(filename=file.filename, data=file.read())
+        db.session.add(upload)
+        db.session.commit()
+        return f'File has been Uploaded : {file.filename}'
+    return render_template('index.html')
